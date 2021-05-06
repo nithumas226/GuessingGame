b70a651ee1880ab52ee3e325b25acd98da534024
nithumas226 <nithumas226@outlook.com>
2021-05-06 12:17:21 -0400

    Add project files.

---
+++ GuessingGame/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace GuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.numberInput = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.outputLabel2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(132, 29);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(231, 32);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Guessing Game";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.descriptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descriptionLabel.ForeColor = System.Drawing.Color.White;
+            this.descriptionLabel.Location = new System.Drawing.Point(116, 79);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(267, 23);
+            this.descriptionLabel.TabIndex = 1;
+            this.descriptionLabel.Text = "Enter a Number from 1 to 100";
+            this.descriptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numberInput
+            // 
+            this.numberInput.Location = new System.Drawing.Point(190, 129);
+            this.numberInput.Name = "numberInput";
+            this.numberInput.Size = new System.Drawing.Size(100, 20);
+            this.numberInput.TabIndex = 2;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(190, 167);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(100, 30);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Guess";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(62, 200);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(346, 41);
+            this.outputLabel.TabIndex = 4;
+            this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputLabel2
+            // 
+            this.outputLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel2.ForeColor = System.Drawing.Color.White;
+            this.outputLabel2.Location = new System.Drawing.Point(68, 276);
+            this.outputLabel2.Name = "outputLabel2";
+            this.outputLabel2.Size = new System.Drawing.Size(340, 47);
+            this.outputLabel2.TabIndex = 5;
+            this.outputLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(502, 450);
+            this.Controls.Add(this.outputLabel2);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.numberInput);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Guessing Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.TextBox numberInput;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label outputLabel2;
+    }
+}
+
---
+++ GuessingGame/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessingGame
+{
+    public partial class Form1 : Form
+    {
+        //Generating a random number and having a place to hold the players guess
+        Random randGen = new Random();
+        int actualValue;
+        public Form1()
+        {
+            InitializeComponent();
+            //Registering the random number as the value needed to guess
+            actualValue = randGen.Next(1, 101);
+            
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            //Creating and registering the players guess
+            int playerGuess;
+            playerGuess = Convert.ToInt32(numberInput.Text);
+
+            //Register absolute number to see how far away the guess is
+            int numAbsolute;
+            numAbsolute = Math.Abs(playerGuess);
+
+            //Determine if the player is too high, low or correct and displaying message
+            if (playerGuess > actualValue)
+            {
+                outputLabel.Text = "Too High";
+            }
+            else if (playerGuess < actualValue)
+            {
+                outputLabel.Text = "Too Low";
+            }
+            else
+            {
+                outputLabel.Text = "Congrats. You Got It";
+            }
+
+        }
+    }
+}
